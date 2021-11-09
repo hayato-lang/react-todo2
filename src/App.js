99dfa5644f5e1b5bf49677c2c582354e617bf169
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-09 14:59:34 +0900

    HTMLを記述

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import { React } from "react";
 import "./styles.css";
 
-export default function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>ああああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+        <ul>
+          <div className="list-row">
+            <li>いいいいいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div className="complete-area"></div>
+      <p className="title">完了したTODO</p>
+      <ul>
+        <div className="list-row">
+          <li>ううううううう</li>
+          <button>戻す</button>
+        </div>
+      </ul>
+    </>
   );
-}
+};
